ffeab7dc19765179df0a3fbd59d0062c4de07037
Kulan66 <kulanthamuditha66@gmail.com>
2025-03-30 10:28:30 +0530

    .

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://back-end-ten-sepia.vercel.app/";
 
 export const api = createApi({
   reducerPath: "api",
